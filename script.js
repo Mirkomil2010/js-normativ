8de342f76e44bcd923c883b33c36700c80653d9b
Mirkomil2010 <mirjalilazizov@gmail.com>
2025-07-17 21:43:30 +0500

    savolar

---
+++ script.js
@@ -0,0 +1,69 @@
+// 1-savol
+// let x = +prompt("Sonizni kiriting")
+// if ( x % 2 === 0){
+//     console.log("x son juft");
+// }else{
+//     console.log("x son toq");
+// }
+
+// 2-savol
+// let x = +prompt("Sonizni kiriting")
+// if ( x > 0){
+//     console.log("x son musbat ");
+// }if(x == 0){
+//     console.log("x son 0ga teng ");
+// }else{
+//     console.log("x son manfiy ");
+// }
+
+// 3-savol
+// let a = +prompt("A sonizni kiriting")
+// let b = +prompt("B sonizni kiriting")
+
+// function check(){
+//     if(a > b){
+//         console.log("A son B sondan katta");
+//     }if(a == b){
+//         console.log("A son B sondan teng");
+//     }else{
+//         console.log("B son A sondan katta");
+//     }
+
+// }
+// check()
+
+// 4-savol
+// let point = prompt("Talabaning olgan balini kiriting");
+
+// if (point >= 1 && point <= 59) {
+//     console.log("Talabaning bahosi 2, imtihondan o'tmadi");
+// } else if (point >= 60 && point <= 69) {
+//     console.log("Talabaning bahosi 3, imtihondan o'tdi");
+// } else if (point >= 70 && point <= 89) {
+//     console.log("Talabaning bahosi 4, imtihondan o'tdi");
+// } else if (point >= 90 && point <= 100) {
+//     console.log("Talabaning bahosi 5, imtihondan o'tdi");
+// } else {
+//     console.log("Noto'g'ri ball kiritildi!");
+// }
+
+// 5-savol
+// function check() {
+//     const togriLogin = "Ustudypupil";
+//     const togriParol = "Frontend";
+
+//     const login = prompt("Loginni kiriting:");
+//     const parol = prompt("Parolni kiriting:");
+
+//     if (login === togriLogin && parol === togriParol) {
+//         console.log("Muvaffaqiyatli");
+//     } else if (login !== togriLogin && parol === togriParol) {
+//         console.log("Kiritilgan login noto'g'ri");
+//     } else if (login === togriLogin && parol !== togriParol) {
+//         console.log("Kiritilgan password noto'g'ri");
+//     } else {
+//         console.log("Kiritilgan login va password noto'g'ri");
+//     }
+// }
+
+// check();
